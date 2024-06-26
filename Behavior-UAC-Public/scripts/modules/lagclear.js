b0b24e3767d76f87407886b00c3073fbf526d0f6
defowler2005 <129338316+defowler2005@users.noreply.github.com>
2024-06-26 23:46:53 +0930

    Update lagclear.js

--- Behavior-UAC-Public/scripts/modules/lagclear.js
+++ Behavior-UAC-Public/scripts/modules/lagclear.js
@@ -1,28 +1,28 @@
 import { scoreTest, setScore } from '../library/utils/score_testing.js';
 import { overworld } from '../library/utils/cmd_queue.js';
 import { tellrawServer } from '../library/utils/prototype.js';
+import { Database } from '../library/Minecraft.js
 
 function lagclear() {
     try {
-        let entitycount = scoreTest('entitycount');
-        let entitycountdown = scoreTest('entityclear');
+        let entitycount = Database.get('entitycount');
+        let entitycountdown = Database.get('entityclear');
 
         if (entitycount >= 145) {
             if (entitycountdown <= 0) {
-                setScore('entityclear', 400, false);
+                Database.set('entityclear', 5);
             }
         }
 
         if (entitycountdown >= 1) {
-            overworld.runCommandAsync(`scoreboard players remove entitydummy entityclear 10`);
-            if (entitycountdown == 350) { tellrawServer(`§¶§cUAC §¶§b► Clearing Entities in §c5`); }
-            if (entitycountdown == 300) { tellrawServer(`§¶§cUAC §¶§b► Clearing Entities in §c4`); }
-            if (entitycountdown == 250) { tellrawServer(`§¶§cUAC §¶§b► Clearing Entities in §c3`); }
-            if (entitycountdown == 200) { tellrawServer(`§¶§cUAC §¶§b► Clearing Entities in §c2`); }
-            if (entitycountdown == 150) { tellrawServer(`§¶§cUAC §¶§b► Clearing Entities in §c1`); }
-            if (entitycountdown == 100) { overworld.runCommandAsync(`function UAC/packages/autolagclearasset`); }
+            if (entitycountdown == 5) { tellrawServer(`§¶§cUAC §¶§b► Clearing Entities in §c5`); }
+            if (entitycountdown == 4) { tellrawServer(`§¶§cUAC §¶§b► Clearing Entities in §c4`); }
+            if (entitycountdown == 3) { tellrawServer(`§¶§cUAC §¶§b► Clearing Entities in §c3`); }
+            if (entitycountdown == 2) { tellrawServer(`§¶§cUAC §¶§b► Clearing Entities in §c2`); }
+            if (entitycountdown == 1) { tellrawServer(`§¶§cUAC §¶§b► Clearing Entities in §c1`); }
+            if (entitycountdown == 0) { overworld.runCommandAsync(`function UAC/packages/autolagclearasset`); }
         }
-    } catch (error) { console.warn(error); }
-}
+    } catch (error) { console.warn(`An error occurrd while clearing lag: ${error}\n${error.stack}`); }
+};
 
 export { lagclear };
