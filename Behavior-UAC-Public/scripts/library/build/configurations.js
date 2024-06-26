d85849475e6bc51b942f9831bec5ee7af6fc00a8
defowler2005 <129338316+defowler2005@users.noreply.github.com>
2024-06-26 22:24:42 +0930

    Update configurations.js

--- Behavior-UAC-Public/scripts/library/build/configurations.js
+++ Behavior-UAC-Public/scripts/library/build/configurations.js
@@ -2,5 +2,5 @@ export const configuration = {
     prefix: 'UAC.', //Default custom command prefix
     staff_tag: 'staffstatus', //Default staffstatus
     uac_version: [2, 8, 8],
-    discord_server: 'https://discord.com/invite/C3NZhphu'
+    discord_server: 'https://discord.gg/wCbwKxQy' //Discord server
 };
